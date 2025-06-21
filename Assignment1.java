d0a855156ec33ab5d367ed63739f32b98901dd85
jesl-gerald <jeslgerald6@gmail.com>
2025-06-21 12:19:15 +0530

    Exercise1 ParaPrinting

---
+++ Assignment1.java
@@ -0,0 +1,7 @@
+class Assignment1
+{
+public static void main(String[] args)
+{
+System.out.print(" We sincerely thank you for your continued partnership with Amazon.in. Your dedication has helped \n make us one of the most visited marketplaces in India. As we continue this journey together, To help \n you better align your products with the most relevant fee categories,\n we're adding 26 new categories and changing referral fees for some existing categories. These changes will take effect on June 30,\n 2025.");
+}
+}
