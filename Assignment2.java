14d2324b54a3e482874ca76fe52697becf5151f3
jesl-gerald <jeslgerald6@gmail.com>
2025-06-21 12:46:03 +0530

    Escape Character execution

---
+++ Assignment2.java
@@ -0,0 +1,7 @@
+class Assignment2
+{
+public static void main (String[] args)
+{
+System.out.print("Name\t Position\tage\nRonaldo\t Striker\t40\nKross\t CMF\t\t35\nNeymar\t Winger\t\t33\nJesl\t AMF\t\t21");
+}
+}
