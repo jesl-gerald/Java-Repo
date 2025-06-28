8e5bf13da8ec0adb04ee71d00721af48ce693a78
jesl-gerald <jeslgerald6@gmail.com>
2025-06-28 20:26:47 +0530

    Sum of Digits

---
+++ Sumofdigits.java
@@ -0,0 +1,15 @@
+class Sumofdigits
+{
+  public static void main(String[] args)
+{
+
+ int a =1;
+ int sum=0;
+ for(a=1;a<=20;a++)
+ {
+   sum=sum+a;
+   System.out.println(sum);
+ }
+  System.out.println("Total sum is =" + sum);
+}
+}
