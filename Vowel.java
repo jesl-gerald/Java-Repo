0e947de73149d57e37fc270f93acc5b6d6b76ded
jesl-gerald <jeslgerald6@gmail.com>
2025-06-29 17:28:19 +0530

    Check the given character is vowel

---
+++ Vowel.java
@@ -0,0 +1,38 @@
+class Vowel
+{
+  public static void main(String[] args)
+{
+  char option = 'u';
+
+  switch(option)
+  {
+     case 'a':
+             System.out.println("It is a vowel");
+             break;
+     
+     case 'e':
+             System.out.println("It is a vowel");
+             break;
+  
+     case 'i':
+             System.out.println("It is a vowel");
+             break;
+
+
+     case 'o':
+             System.out.println("It is a vowel");
+             break;
+     
+     case 'u':
+             System.out.println("It is a vowel");
+             break;
+
+
+     default:
+             System.out.println("It is a consonant");
+
+     }
+  }
+}
+   
+
