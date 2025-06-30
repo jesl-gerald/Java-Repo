6cb265d8120d4ac13f30d8f68e53118e97bd9283
jesl-gerald <jeslgerald6@gmail.com>
2025-06-30 20:15:12 +0530

    Start range & end range - print the numbers b/w this range

---
+++ Range.java
@@ -0,0 +1,16 @@
+class Range
+{
+  public void range(int a)
+  {
+   for(a=1;a<=60;a++)
+   {
+     System.out.println(a);
+   }
+  }
+ 
+   public static void main(String[] args)
+   {
+     Range demo = new Range();
+     demo.range();
+   }
+}
