81c6ec0a971ffeb15cac1628c0cc31a93d851819
jesl-gerald <jeslgerald6@gmail.com>
2025-07-08 12:26:48 +0530

    Comparing two strings

---
+++ Equalsraw.java
@@ -0,0 +1,15 @@
+package foundations;
+
+public class Equalsraw {
+
+	public static void main(String[] args) {
+		
+		String a = "Admin";
+		String b = "Abcde";
+		
+		boolean compareResult = a.equals(b);
+		System.out.println(compareResult);
+
+	}
+
+}
